b93746ac68b19f9dc785af2aa5986e24023f9309
Christine Tran <christinetran825@gmail.com>
2017-10-18 18:34:42 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,5 +12,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171018182616) do
-
+  create_table "users" do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
 end
